0a04a62ab1e987f69e144d574a375986c133eee4
Dima Kurilo <dkurilo@gmail.com>
2021-12-06 17:26:44 -0500

    more

--- day05/app/Main.hs
+++ day05/app/Main.hs
@@ -1,13 +1,13 @@
 module Main where
 
 import Data.Maybe (fromMaybe, listToMaybe)
-import Lib (part1Solution, part2Solution)
+import Lib (isHorVer, isHorVerDiag, part1Solution, part2Solution)
 import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  xs <- getArgs >>= (fmap lines . readFile . fromMaybe "./input" . listToMaybe)
+  xs <- getArgs >>= (fmap (map read . lines) . readFile . fromMaybe "./input" . listToMaybe)
   putStrLn "Part 1"
-  print . part1Solution $ xs
+  print . part1Solution . filter isHorVer $ xs
   putStrLn "Part 2"
-  print . part2Solution $ xs
+  print . part2Solution . filter isHorVerDiag $ xs
