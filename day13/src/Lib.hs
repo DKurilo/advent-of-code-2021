781a8db4a6ff5cafc6c7a0cea221ca871e91c055
Dima Kurilo <dkurilo@gmail.com>
2021-12-14 09:50:37 -0500

    day 14

--- day13/src/Lib.hs
+++ day13/src/Lib.hs
@@ -12,7 +12,6 @@ where
 import Data.List (intercalate)
 import Data.Set (Set (..), findMax, fromList, member, size, union)
 import qualified Data.Set as S
-import Debug.Trace (trace)
 import qualified Text.ParserCombinators.ReadP as P
 import qualified Text.ParserCombinators.ReadPrec as PC
 import Text.Read
@@ -53,19 +52,19 @@ instance Read Fold where
               )
 
 folds :: [Fold] -> Paper -> [Paper]
-folds [] _ = []
-folds (f : fs) (Paper p) = Paper p' : folds fs (Paper p')
+folds fs paper = scanl fold1Time paper fs
   where
-    p' = p1 `union` p2
-    p1 = case f of
-      X x -> S.filter (\(P px _) -> px < x) p
-      Y y -> S.filter (\(P _ py) -> py < y) p
-    p2 = case f of
-      X x -> S.map (\(P px py) -> P (2 * x - px) py) . S.filter (\(P px _) -> px > x) $ p
-      Y y -> S.map (\(P px py) -> P px (2 * y - py)) . S.filter (\(P _ py) -> py > y) $ p
+    fold1Time (Paper p) f = Paper $ p1 `union` p2
+      where
+        p1 = case f of
+          X x -> S.filter (\(P px _) -> px < x) p
+          Y y -> S.filter (\(P _ py) -> py < y) p
+        p2 = case f of
+          X x -> S.map (\(P px py) -> P (2 * x - px) py) . S.filter (\(P px _) -> px > x) $ p
+          Y y -> S.map (\(P px py) -> P px (2 * y - py)) . S.filter (\(P _ py) -> py > y) $ p
 
 part1Solution :: [Fold] -> Paper -> Int
-part1Solution fs = size . (\(Paper p) -> p) . head . folds fs
+part1Solution fs = size . (\(Paper p) -> p) . (!! 1) . folds fs
 
 part2Solution :: [Fold] -> Paper -> String
 part2Solution fs = show . last . folds fs
